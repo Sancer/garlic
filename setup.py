9bfa02de29149f9d809577f36206409c83e46fdf
Uriel Reina <urielreina@gmail.com>
2023-08-14 16:39:52 +0200

    big bang

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='garlic',
+    packages=find_packages(include=['garlic']),
+    version='0.1.0',
+    description='Create a event driven architecture with garlic improve your development experience',
+    author='Uriel Reina Abadía',
+    license='MIT',
+)
---
+++ tests/test_base.py
@@ -0,0 +1,93 @@
+from unittest import TestCase
+from unittest.mock import Mock, MagicMock
+
+from garlic import EventHandler, BaseEvent, EventDispatcher, Subscriber
+
+
+class TestBase(TestCase):
+
+    def setUp(self):
+        self.event_handler = EventHandler()
+        self.event_dispatcher = EventDispatcher(event_handler=self.event_handler)
+
+    def test_subscription_called(self):
+        mock = Mock(wraps=Subscriber())
+        self.event_handler.subscribe(BaseEvent, mock)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+
+
+        mock.assert_called_once_with(event)
+
+    def test_subscription_called_twice_prevent_duplication(self):
+        mock = Mock(wraps=Subscriber())
+        self.event_handler.subscribe(BaseEvent, mock)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+        self.assertRaises(ValueError, self.event_dispatcher, event=event)
+
+        mock.assert_called_with(event)
+        self.assertEqual(mock.call_count, 2)
+
+    def test_subscription_called_twice_with_different_events(self):
+        mock = Mock()
+        self.event_handler.subscribe(BaseEvent, mock)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+
+        mock.assert_called_with(event)
+        self.assertEqual(mock.call_count, 2)
+
+    def test_subscription_called_twice_with_different_events_and_different_subscriptions(self):
+        mock = Mock()
+        self.event_handler.subscribe(BaseEvent, mock)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+
+        mock.assert_called_with(event)
+        self.assertEqual(mock.call_count, 2)
+
+    def test_two_subscriptions_called(self):
+        mock = Mock()
+        mock2 = Mock()
+        self.event_handler.subscribe(BaseEvent, mock)
+        self.event_handler.subscribe(BaseEvent, mock2)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+
+        mock.assert_called_once_with(event)
+        mock2.assert_called_once_with(event)
+
+    def test_two_subscriptions_called_with_different_events(self):
+        mock = Mock()
+        mock2 = Mock()
+        self.event_handler.subscribe(BaseEvent, mock)
+        self.event_handler.subscribe(BaseEvent, mock2)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+
+        mock.assert_called_with(event)
+        mock2.assert_called_with(event)
+        self.assertEqual(mock.call_count, 2)
+        self.assertEqual(mock2.call_count, 2)
+
+    def test_two_subscriptions_called_with_different_events_and_different_subscriptions(self):
+        mock = Mock()
+        mock2 = Mock()
+        self.event_handler.subscribe(BaseEvent, mock)
+        self.event_handler.subscribe(BaseEvent, mock2)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+        event = BaseEvent()
+        self.event_dispatcher(event=event)
+
+        mock.assert_called_with(event)
+        mock2.assert_called_with(event)
+        self.assertEqual(mock.call_count, 2)
+        self.assertEqual(mock2.call_count, 2)
